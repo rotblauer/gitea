add9c859eaa069da26c4e4843abb2ed5586779f0
Mr. Is <isaac.ardis@gmail.com>
2017-02-22 09:34:24 +0100

    $

--- public/js/rb-radio.js
+++ public/js/rb-radio.js
@@ -1,8 +1,9 @@
 
 // Set up music.
 var sound = new Howl({
-    src: ['/music/cocaine-blues.mp3']
+    src: ['/music/cocaine-blues.mp3'],
+    html5: true // Set to true to force HTML5 Audio. This should be used for large audio files so that you don't have to wait for the full file to be downloaded and decoded before playing.
 });
 
 // define short of 'a'
-key('a', function(){ sound.play(); });
+key('shift+4', function(){ sound.play(); });
