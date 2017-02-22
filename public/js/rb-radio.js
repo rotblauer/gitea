18ad9749f61a542dc8be0fa43f4eb940b6391fcc
Mr. Is <isaac.ardis@gmail.com>
2017-02-22 13:42:54 +0100

    pirate radio

--- public/js/rb-radio.js
+++ public/js/rb-radio.js
@@ -1,25 +1,83 @@
-
-// Set up music.
-var sound = new Howl({
-    src: ['/music/cocaine-blues.mp3'],
+var playing = '';
+var soundDefaults = {
     html5: true // Set to true to force HTML5 Audio. This should be used for large audio files so that you don't have to wait for the full file to be downloaded and decoded before playing.
     , volume: 0.5
-});
+};
 
-// define short of 'a'
-key('shift+4', function(){
-    if (sound.playing()) {
-        sound.stop();
+function handleMusicLibrary(fsJSON) {
+
+    // All records established by first letter in filename.
+    // {'j':['folsom.mp3', 'fire.mp3'], 'f':['chicken.mp3', 'road.mp3']}
+    var recordsArchive = {};
+
+    for (var i = 0; i < fsJSON.length; i++) {
+
+        var song = fsJSON[i];
+        var hotkey = song.Name.split("-")[0]; // shift+4-01 - Folsom Prison Blues.mp3
+        console.log(hotkey);
+
+        if (!recordsArchive[hotkey]) {
+            recordsArchive[hotkey] = [];
+            recordsArchive[hotkey].push("/music/" + song.Name);
+        } else {
+            recordsArchive[hotkey].push("/music/" + song.Name);
+        }
     }
-    sound.play();
-});
 
-//turn it up to abc123
-document.onkeypress = function (e) {
-    if (sound.playing()) {
-        var v = sound.volume();
-        if (v < 0.99) {
-            sound.volume(v+0.01);
+    console.log('recordsArchive', recordsArchive);
+
+    // These are the ones that actually play.
+    var jukebox = {};
+
+    for (prop in recordsArchive) {
+        if (recordsArchive.hasOwnProperty(prop)) {
+
+            console.log("recordsArchive[prop]", recordsArchive[prop]);
+
+            var opts = soundDefaults;
+            opts.src = recordsArchive[prop];
+
+            jukebox[prop] = new Howl(opts); // set up default
+
+            jukebox[prop].onplay = function (id) {
+                playing = prop + "-" + id;
+            };
+
+            key(prop, function(){
+                console.log("pressed", prop);
+                if (playing !== '') {
+                    jukebox[playing.split("-")[0]].stop();
+                }
+                jukebox[prop].play();
+            });
         }
     }
 }
+
+$.getJSON("/r/music", handleMusicLibrary);
+
+
+// Protip:
+// cd johnnycash
+// for file in *.mp3; do mv "$file" "j$file"; done
+// rsync --avzLh --progress ./ freya:~/goggable.areteh.co/gogs/public/music/
+
+// {Name: "j01 - Folsom Prison Blues.mp3", Size: 3903488, Mode: 420, ModTime: "2017-02-04T20:10:31+01:00", IsDir: false}
+// 1{Name: "j02 - Busted (Unreleased Bonus Track).mp3", Size: 2035712, Mode: 420, ModTime: "2017-02-04T20:10:31+01:00", IsDir: false}
+// 2{Name: "j03 - Dark as the Dungeon.mp3", Size: 4433920, Mode: 420, ModTime: "2017-02-04T20:10:31+01:00", IsDir: false}
+// 3{Name: "j04 - I Still Miss Someone.mp3", Size: 2349056, Mode: 420, ModTime: "2017-02-04T20:10:33+01:00", IsDir: false}
+// 4{Name: "j05 - Cocaine Blues.mp3", Size: 4354048, Mode: 420, ModTime: "2017-02-04T20:10:37+01:00", IsDir: false}
+// 5{Name: "j06 - 25 Minutes to Go.mp3", Size: 5068800, Mode: 420, ModTime: "2017-02-04T20:10:37+01:00", IsDir: false}
+// 6{Name: "j07 - Orange Blossom Special.mp3", Size: 4343808, Mode: 420, ModTime: "2017-02-04T20:10:39+01:00", IsDir: false}
+// 7{Name: "j08 - The Long Black Veil.mp3", Size: 5705728, Mode: 420, ModTime: "2017-02-04T20:26:01+01:00", IsDir: false}
+// 8{Name: "j09 - Send A Picture of Mother.mp3", Size: 3135488, Mode: 420, ModTime: "2017-02-04T20:10:42+01:00", IsDir: false}
+// 9{Name: "j10 - The Wall.mp3", Size: 2320384, Mode: 420, ModTime: "2017-02-04T20:10:43+01:00", IsDir: false}
+// 10{Name: "j11 - Dirty Old Egg Suckin Dog.mp3", Size: 2168832, Mode: 420, ModTime: "2017-02-04T20:10:43+01:00", IsDir: false}
+// 11{Name: "j12 - Flushed form the Bathroom of Your Heart.mp3", Size: 3291136, Mode: 420, ModTime: "2017-02-04T20:10:45+01:00", IsDir: false}
+// 12{Name: "j13 - Joe Bean (Unreleased Bonus Track).mp3", Size: 3493888, Mode: 420, ModTime: "2017-02-04T20:10:50+01:00", IsDir: false}
+// 13{Name: "j14 - Jackson.mp3", Size: 4620288, Mode: 420, ModTime: "2017-02-04T20:10:51+01:00", IsDir: false}
+// 14{Name: "j15 - Give My Love to Rose.mp3", Size: 3858432, Mode: 420, ModTime: "2017-02-04T20:10:51+01:00", IsDir: false}
+// 15{Name: "j16 - I Got Stripes.mp3", Size: 2811904, Mode: 420, ModTime: "2017-02-04T20:10:58+01:00", IsDir: false}
+// 16{Name: "j17 - The Legend of Henrys Hammer (Unreleased Bonus Track).mp3", Size: 10283008, Mode: 420, ModTime: "2017-02-04T20:25:27+01:00", IsDir: false}
+// 17{Name: "j18 - Green Green Grass of Home.mp3", Size: 3592192, Mode: 420, ModTime: "2017-02-04T20:10:56+01:00", IsDir: false}
+// 18{Name: "j19 - Greystone Chapel.mp3", Size: 8695808, Mode: 420, ModTime: "2017-02-04T20:25:52+01:00", IsDir: false}
---
+++ public/js/rb-radio2.js
@@ -0,0 +1,165 @@
+/*!
+ *  Howler.js Audio Player Demo
+ *  howlerjs.com
+ *
+ *  (c) 2013-2016, James Simpson of GoldFire Studios
+ *  goldfirestudios.com
+ *
+ *  MIT License
+ */
+
+/**
+ * Player class containing the state of our playlist and where we are in it.
+ * Includes all methods for playing, skipping, updating the display, etc.
+ * @param {Array} playlist Array of objects with playlist song details ({file, howl}).
+ */
+var Player = function(playlist) {
+    this.playlist = playlist;
+    this.index = 0;
+    $("#songs-list").html('');
+
+    // Setup the playlist display.
+    playlist.forEach(function(song) {
+        var div = $('<div></div>');
+        div.addClass('list-song');
+        div.html(song.file);
+        div.on("click", function () {
+            player.skipTo(playlist.indexOf(song));
+            $(".list-song").removeClass("playing");
+            div.addClass("playing");
+        });
+        $("#songs-list").append(div);
+    });
+};
+
+Player.prototype = {
+    /**
+     * Play a song in the playlist.
+     * @param  {Number} index Index of the song in the playlist (leave empty to play the first or current).
+     */
+    play: function(index) {
+        var self = this;
+        var sound;
+
+        index = typeof index === 'number' ? index : self.index;
+        var data = self.playlist[index];
+
+        // If we already loaded this track, use the current one.
+        // Otherwise, setup and load a new Howl.
+        if (data.howl) {
+            sound = data.howl;
+        } else {
+            sound = data.howl = new Howl({
+                src: ['/music/' + data.file],
+                html5: true, // Force to HTML5 so that the audio can stream in (best for large files).
+                onend: function() {
+                    self.skipTo(index + 1);
+                }
+            });
+        }
+
+        // Begin playing the sound.
+        sound.play();
+
+        // // Update the track display.
+        // track.innerHTML = (index + 1) + '. ' + data.title;
+
+        // Keep track of the index we are currently playing.
+        self.index = index;
+    },
+
+    /**
+     * Skip to the next or previous track.
+     * @param  {String} direction 'next' or 'prev'.
+     */
+    skip: function(direction) {
+        var self = this;
+
+        // Get the next track based on the direction of the track.
+        var index = 0;
+        if (direction === 'prev') {
+            index = self.index - 1;
+            if (index < 0) {
+                index = self.playlist.length - 1;
+            }
+        } else {
+            index = self.index + 1;
+            if (index >= self.playlist.length) {
+                index = 0;
+            }
+        }
+
+        self.skipTo(index);
+    },
+
+    /**
+     * Skip to a specific track based on its playlist index.
+     * @param  {Number} index Index in the playlist.
+     */
+    skipTo: function(index) {
+        var self = this;
+
+        // Stop the current track.
+        if (self.playlist[self.index].howl) {
+            self.playlist[self.index].howl.stop();
+        }
+
+        // Play the new track.
+        self.play(index);
+    }
+};
+
+var player;
+
+$(function () {
+
+    $.getJSON("/r/music", function(files) {
+        var playables = [];
+        for (var i = 0; i < files.length; i++) {
+            playables.push({
+                file: files[i].Name,
+                howl: null
+            });
+        }
+        player = new Player(playables);
+    });
+
+    var songsSearcher = $("input#songs-filterer");
+    songsSearcher.keyup(function (e) {
+        var $this = $(this);
+        var keyword = $this.val();
+
+        if (keyword.length > 1) {
+            $(".list-song").each(function (i, el) {
+                if ($(this).text().toLowerCase().indexOf(keyword.toLowerCase()) < 0) {
+                    $(this).hide();
+                }
+            });
+        } else {
+            $(".list-song").show();
+        }
+    });
+});
+
+// // Setup our new audio player class and pass it the playlist.
+// var player = new Player([
+//   {
+//     file: 'rave_digger',
+//     howl: null
+//   },
+//   {
+//     file: '80s_vibe',
+//     howl: null
+//   },
+//   {
+//     file: 'running_out',
+//     howl: null
+//   }
+// ]);
+
+// prevBtn.addEventListener('click', function() {
+//     player.skip('prev');
+// });
+// nextBtn.addEventListener('click', function() {
+//     player.skip('next');
+// });
