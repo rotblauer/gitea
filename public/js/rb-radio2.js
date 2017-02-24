7057fc7d30f7b6245c63804edbf57de18b558065
Mr. Is <isaac.ardis@gmail.com>
2017-02-24 19:13:34 +0100

    :floppy_disk:

--- public/js/rb-radio2.js
+++ public/js/rb-radio2.js
@@ -58,7 +58,11 @@ Player.prototype = {
         // Stops ANY track in its tracks.
         // Keeps cache thingey size from not 1G.
         for (var x = 0; x < self.playlist.length; x++) {
-            if (x !== index && self.playlist[x].howl) {
+            if (x !== index &&
+                x !== self.index &&
+                x !== index+1 &&
+                self.playlist[x].howl &&
+                ( self.playlist[x].howl.state() === "loaded" || self.playlist[x].howl.state() === "loading" )) {
                 self.playlist[x].howl.unload();
             }
         }
